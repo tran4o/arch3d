dea36ad690fcb6ec982eb5694de5133a2d86582d
Martin Hristov <martin.hristov@plan-vision.com>
2015-06-17 22:48:01 +0300

    sdasd

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -19,9 +19,63 @@ exports.Main = Component.specialize(/** @lends Main# */ {
 	{ 
 		value: function(event) 
 		{
+			//------------------------------------------------------
+			function onClick(event) 
+			{
+				if (event && event.target && event.target.name) 
+				{
+					console.log("SELECT BY ID '"+name+"'");
+					event.preventDefault();
+					event.stopPropagation();
+					window.SELECTED=event.target.name;
+					MAIN.templateObjects.sceneView.needsDraw=true;
+				}
+			}
+			//------------------------------------------------------
+			
 			this.templateObjects.sceneView.stop();
 			this.templateObjects.sceneView.viewPoint = event.target.viewPoint;
-			this.templateObjects.sceneView.allowsViewPointControl = true; //true; //event.target.viewPoint === this.templateObjects.planetVP;					
+			this.templateObjects.sceneView.allowsViewPointControl = true; //true; //event.target.viewPoint === this.templateObjects.planetVP;
+			var id = event.target.viewPoint.id;
+			var that=this;
+			function onDone(html) {
+				that.__cached[id]=html;
+				document.querySelector(".html_details").innerHTML=html;
+			}
+			if (!this.__cached)
+				this.__cached={};
+			if (this.__cached[id]) {
+				onDone(this.__cached[id]);				
+			} else {
+				var xmlhttp;
+				var url="html/"+id+".html";
+				if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+					xmlhttp=new XMLHttpRequest();
+				} else { // code for IE6, IE5
+					xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				xmlhttp.onreadystatechange = function()
+				{
+				  if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
+				    document.getElementById("status").style.display="block";
+				    document.getElementById("status").innerHTML=url+"&nbsp;&nbsp;|&nbsp;&nbsp;"+xmlhttp.status;
+				  }
+				  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) 
+				  {
+				    onDone(xmlhttp.responseText);
+				    setTimeout(function() {
+						var sel = document.querySelectorAll(".link");
+						for (var i=0;i<sel.length;i++)
+							sel[i].onclick=onClick;
+				    },0);
+				  }
+				}
+				xmlhttp.open("GET", url, true);
+				xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+				xmlhttp.setRequestHeader("Cache-Control", "no-cache"); // For no cache
+				xmlhttp.send();
+			}
+			
 		}
 	},    
 	handleTESTAction: {
